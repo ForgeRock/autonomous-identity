c2cacba43d4187554e9aaef5854defcce6e9bd97
Ashwath Akirekadu <ashwath.akirekadu@forgerock.com>
2023-09-03 15:52:59 -0700

    Initial commit

---
+++ k8/common/openidm/base/idm-iga-api-handler.js
@@ -0,0 +1,91 @@
+/**
+ * Script to be placed in IDM script directory to allow the IGA API to be called an authenticated in a simple and readable fashion 
+ * within IDM workflows and scripts.
+ * 
+ * Example:  var result = openidm.action('iga/governance/certification/:id', 'GET')
+ * Example:  var result = openidm.action('iga/governance/applications/<id>/activate', 'POST', {})
+ */
+
+run();
+
+function run() {
+    var path = request.resourcePath;
+    var body = request.content;
+    var method = request.action;
+    var params = request.additionalParameters;
+
+    if (!path) {
+        logger.warn("No path provided to call IGA API.")
+        return;
+    }
+
+    if (!method) {
+        logger.warn("Must provide a request method as the 'action' when calling an IGA API.");
+        return;
+    }
+    else {
+        method = method.toUpperCase();
+    }
+
+    return makeExternalCall(path, method, body, params)
+}
+
+function generateUrl(path, queryParams) {
+    var url = 'http://iga-api:3005/iga/' + path;
+    if (queryParams != null && Object.keys(queryParams).length > 0) {
+        var params = '';
+        if (typeof queryParams === 'object') {
+            var paramsList = [];
+            Object.keys(queryParams).forEach(function(key) {
+                paramsList.push(key + '=' + queryParams[key])
+            })
+            params = paramsList.join('&');
+        }
+        else {
+            params = queryParams;
+        }
+        url = url + '?' + params;
+    }
+    return url;
+}
+
+function makeExternalCall(path, method, body, queryParams) {
+    // Get authorization header to allow call to be made from IDM
+    var url = generateUrl(path, queryParams);
+    var date = new Date().toUTCString();
+    var header = getAuthorizationHeaderForIGA(date);
+    if (!header) {
+        logger.warn("Could not generate authorization header.");
+        return;
+    }
+
+    var requestBody = body.requestBody || body;
+
+    var params = {
+        url: url,
+        method: method,
+        headers : {
+        Date : date,
+        Authorization : header
+        },
+        contentType: 'application/json',
+        body: org.forgerock.json.JsonValue.json(requestBody).toString(),
+      }
+      var result = openidm.action("external/rest", "call", params);
+      return result;
+}
+
+function getAuthorizationHeaderForIGA(date) {
+    const mac = javax.crypto.Mac.getInstance("HmacSHA256");
+    var secret = new java.lang.String("hmacsecret");
+    var secretBytes = secret.getBytes(java.nio.charset.StandardCharsets.UTF_8);
+    var secretKeySpec = new javax.crypto.spec.SecretKeySpec(secretBytes,
+                        "HmacSHA256");
+    mac.init(secretKeySpec);
+    const currentDateTimeString =  "Date: " + date + "\n"; 
+    new java.lang.String(currentDateTimeString).getBytes(java.nio.charset.StandardCharsets.UTF_8);
+    var signedHash = mac.doFinal(new java.lang.String(currentDateTimeString).getBytes(java.nio.charset.StandardCharsets.UTF_8));
+    var base64Hash = java.util.Base64.getEncoder().encodeToString(signedHash);
+    const authorizationHeader = 'Signature keyId="service1-hmac",algorithm="hmac-sha256",headers="date ", signature="' + base64Hash + '"';
+    return authorizationHeader;
+}
---
+++ k8/iga/init.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Usage:
+# For Datastore:
+# ./init.sh "$IGA_DEPLOYMENT_NAME"
+# For BigTable:
+# ./init.sh "$IGA_DEPLOYMENT_NAME" bigtable
+
+echo " "
+cat readme.txt
+echo " "
+
+if [ "$#" -eq 0 ]; then
+  echo " "
+  echo "Usage: init.sh <tenant-name>"
+  echo " "
+  echo "    example: init.sh panduri-iga-0821 iga"
+  echo " "
+  exit 1
+fi
+
+TENANT_NAME=$1
+DATABASE=${2:-datastore}
+BASEDIR=$(cd ../ && pwd)
+
+source ./config.env
+
+grep -rl --exclude='*.sh' '#NAMESPACE#' $BASEDIR | xargs sed -ibackup "s|#NAMESPACE#|${NAMESPACE}|"
+sed -ibackup "s|<ADD HERE>|${TENANT_NAME}|"  utils/iga_datastore_schema_job.yaml
+sed -ibackup "s|<ADD HERE>|${TENANT_NAME}|"  utils/iga_bigtable_schema_job.yaml
+sed -ibackup "s|#BIG_TABLE_PROJECT_ID#|${GCP_PROJECT_ID}|"          utils/iga_bigtable_schema_job.yaml
+sed -ibackup "s|#BIG_TABLE_INSTANCE_ID#|${BIG_TABLE_INSTANCE_ID}|"  utils/iga_bigtable_schema_job.yaml
+sed -ibackup "s|#BIG_TABLE_ENABLED#|true|"                          utils/iga_bigtable_schema_job.yaml
+
+kubectl create namespace $NAMESPACE
+
+kubectl -n $NAMESPACE create serviceaccount iga-workload-identity
+kubectl -n $NAMESPACE annotate serviceaccount iga-workload-identity iam.gke.io/gcp-service-account=${WORKLOAD_IDENTITY_SERVICE_ACCOUNT}
+pushd ../secrets
+export NAMESPACE=${NAMESPACE}
+sh ./apply-secrets.sh
+popd
+
+echo "Preferred database: ${DATABASE}"
+if [[ $DATABASE == "bigtable" ]]; then
+    printf "Using ${YELLOW}BigTable database${COLOR_OFF} for JAS...\n"
+    kubectl -n $NAMESPACE create -f utils/iga_bigtable_schema_job.yaml
+    echo "Waiting for BigTable schema job to start..."
+    sleep 90
+    kubectl -n $NAMESPACE logs -f --ignore-errors=true job/iga-datastore-schema-job
+    echo " "
+    echo "--------"
+else
+    printf "Using ${YELLOW}Cloud Datastore database${COLOR_OFF} for JAS...\n"
+    kubectl -n $NAMESPACE create -f utils/iga_datastore_schema_job.yaml
+    echo "Waiting for Datastore schema job to start..."
+    sleep 60
+    kubectl -n $NAMESPACE logs -f --ignore-errors=true job/iga-datastore-schema-job
+    echo " "
+    echo "--------"
+fi
+
+echo "Next steps: "
+echo "  * copy 'Tenant ID' from above logs"  
+echo "  * set it to JAS_TENANT_ID property in config.env file"
+echo "  * run ./deploy.sh"
+echo "--------"
+echo " "
